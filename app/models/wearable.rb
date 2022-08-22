c59d985151e8745ae4ccfe619aa6c3e12bf11fb7
Marius van Remundt <marius_v_r@hotmail.com>
2022-08-22 16:42:04 +0200

    added db:seed and added validations

--- app/models/wearable.rb
+++ app/models/wearable.rb
@@ -1,4 +1,6 @@
 class Wearable < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :brand, :category, :price, :description, :title, presence: true
+  validates :category, inclusion: { in: ["watch", "jacket", "glassed"]}
 end
