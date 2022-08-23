859e14e17b4ed9a3be4ab9ccdbce6d4d9b5c4a6c
Tan Akpek akpektan@gmail.com <akpektan@gmail.com>
2022-08-23 23:04:58 +0200

    calendar done once authorization policy set

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,4 +9,26 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @bookings_for_wearable = Booking.where(wearable: @wearable, status: 'confirmed')
   end
+
+  def create
+    @wearable = Wearable.find_by_id(params[:wearable_id])
+    @booking = Booking.new(params_booking)
+    @booking.status = "unconfirmed"
+    @booking.user = current_user
+    @booking.wearable = @wearable
+
+    @booking.save
+    authorize @booking
+    if @booking.save
+      redirect_to new_wearable_booking_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_booking
+    params.require(:booking).permit(:start_date, :end_date, :wearable_id)
+  end
 end
--- app/models/wearable.rb
+++ app/models/wearable.rb
@@ -1,6 +1,13 @@
 class Wearable < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
+
   validates :brand, :category, :price, :description, :title, :size, :photos, presence: true
   validates :category, inclusion: { in: ["watch", "jacket", "glassed"]}
   has_many_attached :photos
