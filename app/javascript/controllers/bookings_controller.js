88838ca9492114abb12457f4793c61b06fcbec36
Tan Akpek akpektan@gmail.com <akpektan@gmail.com>
2022-08-23 16:44:37 +0200

    calendar

---
+++ app/javascript/controllers/bookings_controller.js
@@ -0,0 +1,15 @@
+
+
+
+import { Controller } from "@hotwired/stimulus"
+export default class extends Controller {
+
+
+  connect() {
+    console.log("movies controller connected")
+  }
+
+  //add(event){
+   // console.log(document.querySelectorAll('.day'));
+  //}
+}
