355f080c5334da49129fe0a6411a5c6faabf02b6
Tan Akpek akpektan@gmail.com <akpektan@gmail.com>
2022-08-23 12:57:05 +0200

    navbar link update '

--- app/models/wearable.rb
+++ app/models/wearable.rb
@@ -1,7 +1,7 @@
 class Wearable < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :brand, :category, :price, :description, :title, :size, presence: true
+  validates :brand, :category, :price, :description, :title, :size, :photos, presence: true
   validates :category, inclusion: { in: ["watch", "jacket", "glassed"]}
   has_many_attached :photos
 
