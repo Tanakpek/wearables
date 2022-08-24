2644f22355085ac6def0e9f454ea508c2abd8a1e
Tan Akpek akpektan@gmail.com <akpektan@gmail.com>
2022-08-24 17:58:21 +0200

    booking feature completed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,38 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 import { Application } from "@hotwired/stimulus"
-import {flatpickr} from "flatpickr";
-import {init_flatpickr } from "./plugins/init_flatpickr";
+import "flatpickr";
+
+import { Flatpicker } from "./plugins/init_flatpickr";
+
+document.addEventListener('turbo:load', function(){
+  console.log("from app")
+  console.log("hello from flatpicker")
+  const startDateInput = document.getElementById('booking_start_date');
+  const endDateInput = document.getElementById('booking_end_date');
+  if (startDateInput) {
+  const unavailableDates = JSON.parse(document.querySelector('#flat-booking-dates').dataset.unavailable)
+  endDateInput.disabled = true
+
+  flatpickr(startDateInput, {
+    minDate: "today",
+    disable: unavailableDates,
+    dateFormat: "Y-m-d",
+  });
+
+  console.log('im in the file')
+
+  startDateInput.addEventListener("change", (e) => {
+    if (startDateInput != "") {
+      endDateInput.disabled = false
+    }
+    flatpickr(endDateInput, {
+      minDate: e.target.value,
+      disable: unavailableDates,
+      dateFormat: "Y-m-d"
+      });
+    })
+  };
+ });
 
   // awesome code
