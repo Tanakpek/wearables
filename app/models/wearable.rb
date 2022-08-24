6099a6d21d04ef85f3ac069f53e4bd3a5c96d5b2
Marius van Remundt <marius_v_r@hotmail.com>
2022-08-24 11:18:42 +0200

    -done

--- app/models/wearable.rb
+++ app/models/wearable.rb
@@ -1,6 +1,6 @@
 class Wearable < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :brand, :category, :price, :description, :title, :size, :photos, presence: true
   validates :category, inclusion: { in: ["watch", "jacket", "glassed"]}
   has_many_attached :photos
